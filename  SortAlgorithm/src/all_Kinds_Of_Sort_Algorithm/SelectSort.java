929179e175ac26f50b66a9b6314655988c470e01
magicmmp <2233973404@qq.com>
2019-10-03 14:11:16 +0800

    auto git push  Java eclipse project: 2019年10月 3日 14:11:16

---
+++  SortAlgorithm/src/all_Kinds_Of_Sort_Algorithm/SelectSort.java
@@ -0,0 +1,50 @@
+package all_Kinds_Of_Sort_Algorithm;
+
+/**
+  * 选择排序：每次找出最小的元素，将其放在前面。
+ * 
+ *
+ */
+
+public class SelectSort 
+{
+	
+	public static void sort(int[] a) 
+	{
+		int minIndex;
+		int tmp;
+		int i,j;
+		for(i=0;i<a.length-1;i++)  //1、i是每趟选择的开始位置
+		{
+			minIndex=i;
+			for(j=i+1;j<a.length;j++)
+			{
+				if(a[j]<a[minIndex]) //2、找出从a[i]到最后一个元素之间的最小的元素
+					minIndex=j;
+			}
+			if(minIndex!=i) //3、将每趟选出的最小值放在前面
+			{
+				tmp=a[i];
+				a[i]=a[minIndex];
+				a[minIndex]=tmp;
+			}
+		}
+	}
+	
+	
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		int a[]= {5,4,9,8,7,6,0,1,3,2};
+		sort(a);
+		for (int j = 0; j < a.length; j++) 
+		{
+			System.out.print(a[j]+" ");
+		}
+		System.out.println("\n");
+
+	}
+
+}
