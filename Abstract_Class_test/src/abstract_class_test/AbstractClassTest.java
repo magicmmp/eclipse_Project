ecc7ae394620b16deffb86ead6cb69c82281fbf5
magicmmp <2233973404@qq.com>
2020-02-12 11:09:38 +0800

    auto git push  Java eclipse_Project: 2020年02月12日 11:09:38

---
+++ Abstract_Class_test/src/abstract_class_test/AbstractClassTest.java
@@ -0,0 +1,14 @@
+package abstract_class_test;
+
+public class AbstractClassTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//引用是Shape类型，但是实例是Circle类型
+		Shape shape = new Circle("黄色", 1.5);
+		System.out.println(shape.getPerimeter());
+		System.out.println(shape.getType());
+
+	}
+
+}
--- JavaDoc_test/src/JavaDocTest.java
+++ JavaDoc_test/src/JavaDocTest.java
@@ -11,15 +11,40 @@ public class JavaDocTest {
 	/**
 	 * 表示人名字
 	 */
-	protected String name;
+	protected String name;//怎么用不了此变量？
+	
+	void jump()
+	{
+		System.out.println("正在执行jump方法。");
+	}
+	
+	void run(String...books)
+	{
+		for(String book :books)
+			System.out.println(book);
+		this.jump();
+		System.out.println("正在执行run方法。");
+	}
 
 	/**
 	 * 主方法，表示程序的入口
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hhehhe");
+	
+		int[] intArray=new int[5];
+		for(int i=0;i<intArray.length;i++)
+			intArray[i]=i*2;
+		for(int e:intArray)
+		{
+			System.out.println(e);
+		}
+		
+		JavaDocTest pDocTest=new JavaDocTest();
+		pDocTest.run();
+		
+		
+		
 	}
 
 }
